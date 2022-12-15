0d521bd5ac3a4063168160af0ada6ca3c7ddfa80
hau8477 <hau8477@gmail.com>
2022-12-15 16:48:21 +0700

    first commit

--- src/mysql/ss1_relational_database/exercise/students_management.sql
+++ src/mysql/ss1_relational_database/exercise/students_management.sql
@@ -1,37 +1,35 @@
-create database students_management;
+create
+database students_management;
 
-use students_management;
+use
+students_management;
 
-create table class(
-  id int, 
-  name varchar(45)
+create table class
+(
+    id   int,
+    name varchar(45)
 );
 
-create table teacher(
-  id int, 
-  name varchar(45), 
-  age int, 
-  country varchar(45)
+create table teacher
+(
+    id      int,
+    name    varchar(45),
+    age     int,
+    country varchar(45)
 );
 
-insert into class(id, name) 
-values 
-  (1, 'Ngoc Hau'), 
-  (2, 'Van Hung'), 
-  (3, 'Duc Vu'), 
-  (4, 'Ngoc Vu');
-  
-insert into teacher(id, name, age, country) 
-values 
-  (1, 'Chanh Tran', 37, 'Viet Nam'), 
-  (2, 'Quang NN', 27, 'Viet Nam');
-  
-select 
-  * 
-from 
-  Class;
-  
-select 
-  * 
-from 
-  Teacher;
+insert into class(id, name)
+values (1, 'Ngoc Hau'),
+       (2, 'Van Hung'),
+       (3, 'Duc Vu'),
+       (4, 'Ngoc Vu');
+
+insert into teacher(id, name, age, country)
+values (1, 'Chanh Tran', 37, 'Viet Nam'),
+       (2, 'Quang NN', 27, 'Viet Nam');
+
+select *
+from Class;
+
+select *
+from Teacher;
