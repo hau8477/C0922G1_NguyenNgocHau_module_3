c0ee83c82f055ef5936d8c6fd6ee390806b000a9
hau8477 <hau8477@gmail.com>
2022-12-15 16:46:17 +0700

    first commit

--- src/mysql/ss2_design_database/exercise/convert_erd_to_relational/convert_erd_to_relational_model.sql
+++ src/mysql/ss2_design_database/exercise/convert_erd_to_relational/convert_erd_to_relational_model.sql
@@ -1,66 +1,77 @@
-create database convert_erd_to_relational;
+create
+database convert_erd_to_relational;
 
-use convert_erd_to_relational;
+use
+convert_erd_to_relational;
 
-create table phieu_xuat(
-  so_px int primary key,
-  ngay_xuat datetime
+create table phieu_xuat
+(
+    so_px     int primary key,
+    ngay_xuat datetime
 );
 
-create table vat_tu(
-  ma_vat_tu int primary key, 
-  ten_vat_tu varchar(50)
+create table vat_tu
+(
+    ma_vat_tu  int primary key,
+    ten_vat_tu varchar(50)
 );
 
-create table phieu_nhap(
-  so_pn int primary key,
-  ngay_nhap datetime
+create table phieu_nhap
+(
+    so_pn     int primary key,
+    ngay_nhap datetime
 );
 
-create table nha_cung_cap(
-  ma_ncc int primary key, 
-  ten_ncc varchar(50), 
-  dia_chi varchar(50)
+create table nha_cung_cap
+(
+    ma_ncc  int primary key,
+    ten_ncc varchar(50),
+    dia_chi varchar(50)
 );
 
-create table don_dat_hang(
-  so_dh int primary key, 
-  ngay_dh datetime, 
-  ma_ncc int, 
-  foreign key(ma_ncc) references nha_cung_cap(ma_ncc)
+create table don_dat_hang
+(
+    so_dh   int primary key,
+    ngay_dh datetime,
+    ma_ncc  int,
+    foreign key (ma_ncc) references nha_cung_cap (ma_ncc)
 );
 
-create table so_dien_thoai(
-  ma_sdt int primary key, 
-  ten_ncc_sdt varchar(50), 
-  ma_ncc int, 
-  foreign key(ma_ncc) references nha_cung_cap(ma_ncc)
+create table so_dien_thoai
+(
+    ma_sdt      int primary key,
+    ten_ncc_sdt varchar(50),
+    ma_ncc      int,
+    foreign key (ma_ncc) references nha_cung_cap (ma_ncc)
 );
 
-create table xuat_vat_tu(
-  so_px int, 
-  ma_vat_tu int, 
-  don_gia_xuat int, 
-  so_luong_xuat int, 
-  primary key(so_px, ma_vat_tu), 
-  foreign key(so_px) references phieu_xuat(so_px), 
-  foreign key(ma_vat_tu) references vat_tu(ma_vat_tu)
+create table xuat_vat_tu
+(
+    so_px         int,
+    ma_vat_tu     int,
+    don_gia_xuat  int,
+    so_luong_xuat int,
+    primary key (so_px, ma_vat_tu),
+    foreign key (so_px) references phieu_xuat (so_px),
+    foreign key (ma_vat_tu) references vat_tu (ma_vat_tu)
 );
 
-create table nhap_vat_tu(
-  so_pn int, 
-  ma_vat_tu int, 
-  don_gia_nhap int, 
-  so_luong_nhap int, 
-  primary key(so_pn, ma_vat_tu), 
-  foreign key(so_pn) references phieu_nhap(so_pn), 
-  foreign key(ma_vat_tu) references vat_tu(ma_vat_tu)
+create table nhap_vat_tu
+(
+    so_pn         int,
+    ma_vat_tu     int,
+    don_gia_nhap  int,
+    so_luong_nhap int,
+    primary key (so_pn, ma_vat_tu),
+    foreign key (so_pn) references phieu_nhap (so_pn),
+    foreign key (ma_vat_tu) references vat_tu (ma_vat_tu)
 );
 
-create table don_hang_vat_tu(
-  ma_vat_tu int, 
-  so_dh int, 
-  primary key(ma_vat_tu, so_dh), 
-  foreign key(ma_vat_tu) references vat_tu(ma_vat_tu), 
-  foreign key(so_dh) references don_dat_hang(so_dh)
+create table don_hang_vat_tu
+(
+    ma_vat_tu int,
+    so_dh     int,
+    primary key (ma_vat_tu, so_dh),
+    foreign key (ma_vat_tu) references vat_tu (ma_vat_tu),
+    foreign key (so_dh) references don_dat_hang (so_dh)
 );
--- src/mysql/ss2_design_database/exercise/products_management/products_management.sql
+++ src/mysql/ss2_design_database/exercise/products_management/products_management.sql
@@ -1,32 +1,38 @@
-create database products_management;
+create
+database products_management;
 
-use products_management;
+use
+products_management;
 
-create table customer(
-  customer_id int auto_increment primary key, 
-  customer_name varchar(50), 
-  customer_age int
+create table customer
+(
+    customer_id   int auto_increment primary key,
+    customer_name varchar(50),
+    customer_age  int
 );
 
-create table `order`(
-  order_id int auto_increment primary key, 
-  customer_id int unique not null, 
-  order_date datetime, 
-  order_total_price double, 
-  foreign key(customer_id) references customer(customer_id)
+create table `order`
+(
+    order_id          int auto_increment primary key,
+    customer_id       int unique not null,
+    order_date        datetime,
+    order_total_price double,
+    foreign key (customer_id) references customer (customer_id)
 );
 
-create table product(
-  product_id int auto_increment primary key, 
-  product_name varchar(50), 
-  product_price double
+create table product
+(
+    product_id    int auto_increment primary key,
+    product_name  varchar(50),
+    product_price double
 );
 
-create table order_detail(
-  order_id int auto_increment, 
-  product_id int, 
-  order_qty double, 
-  primary key(order_id, product_id), 
-  foreign key(order_id) references `order`(order_id), 
-  foreign key(product_id) references product(product_id)
+create table order_detail
+(
+    order_id   int auto_increment,
+    product_id int,
+    order_qty  double,
+    primary key (order_id, product_id),
+    foreign key (order_id) references `order` (order_id),
+    foreign key (product_id) references product (product_id)
 );
