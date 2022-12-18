87afcac66993b93a28ea142d5e58c0f2af5ed552
hau8477 <hau8477@gmail.com>
2022-12-18 19:34:48 +0700

    first commit

--- case_study/database/query_02_05.sql
+++ case_study/database/query_02_05.sql
@@ -3,6 +3,7 @@ furama_resort;
 
 -- 2.  Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các
 --  ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
+
 SELECT *
 FROM nhan_vien nv
 WHERE substring_index(nv.ho_ten, ' ', 1) LIKE 'H%'
@@ -12,6 +13,7 @@ WHERE substring_index(nv.ho_ten, ' ', 1) LIKE 'H%'
 
 -- 3.  Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà 
 -- Nẵng” hoặc “Quảng Trị”.
+
 SELECT *
 FROM khach_hang kh
 WHERE (
@@ -28,6 +30,7 @@ WHERE (
 -- 4.  Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được 
 -- sắp xếp tăng dần theo số lần đặt phòng của khách hàng. Chỉ đếm những khách hàng nào có Tên loại 
 -- khách hàng là “Diamond”.
+
 SELECT kh.ma_khach_hang,
        kh.ho_ten,
        COUNT(hd.ma_hop_dong) AS so_lan_dat_phong
@@ -44,6 +47,7 @@ ORDER BY so_lan_dat_phong;
 --  với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet)
 --  cho tất cả các khách hàng đã từng đặt phòng.
 --  (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+
 SELECT kh.ma_khach_hang,
        kh.ho_ten,
        lk.ten_loai_khach,
