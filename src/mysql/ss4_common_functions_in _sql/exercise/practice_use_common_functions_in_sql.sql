6d2b702eecc43410d5cccea20d214566d0191911
hau8477 <hau8477@gmail.com>
2022-12-19 11:58:27 +0700

    first commit

--- src/mysql/ss4_common_functions_in _sql/exercise/practice_use_common_functions_in_sql.sql
+++ src/mysql/ss4_common_functions_in _sql/exercise/practice_use_common_functions_in_sql.sql
@@ -1,21 +1,23 @@
-USE 
+USE
 student_management;
 
 -- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
 
-SELECT * 
+SELECT *
 FROM subject sub
 WHERE sub.credit LIKE (SELECT MAX(credit) FROM subject);
 
 -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
 
 SELECT sub.*
-FROM subject sub JOIN mark m ON sub.sub_id = m.sub_id
+FROM subject sub
+         JOIN mark m ON sub.sub_id = m.sub_id
 WHERE m.mark LIKE (SElECT MAX(mark) FROM mark);
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 
 SELECT s.*, AVG(m.mark)
-FROM student s JOIN mark m ON s.student_id = m.student_id
+FROM student s
+         JOIN mark m ON s.student_id = m.student_id
 GROUP BY s.student_id
 ORDER BY AVG(m.mark) DESC;
