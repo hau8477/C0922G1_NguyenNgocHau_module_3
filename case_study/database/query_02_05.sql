fb61577204ac66bf7e201b4025a2385c4719d9ba
hau8477 <hau8477@gmail.com>
2022-12-20 00:20:45 +0700

    first commit

--- case_study/database/query_02_05.sql
+++ case_study/database/query_02_05.sql
@@ -6,19 +6,19 @@ furama_resort;
 
 SELECT *
 FROM nhan_vien
-WHERE 	
-		substring_index(ho_ten,' ',-1) LIKE 'H%' OR
-		substring_index(ho_ten,' ',-1) LIKE 'T%' OR
-		substring_index(ho_ten,' ',-1) LIKE 'K%' AND
-        CHARACTER_LENGTH(ho_ten) <= 15;
+WHERE substring_index(ho_ten, ' ', -1) LIKE 'H%'
+   OR substring_index(ho_ten, ' ', -1) LIKE 'T%'
+   OR substring_index(ho_ten, ' ', -1) LIKE 'K%' AND
+      CHARACTER_LENGTH(ho_ten) <= 15;
 
 -- 3.  Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà 
 -- Nẵng” hoặc “Quảng Trị”.
 
 SELECT *
 FROM khach_hang
-WHERE dia_chi LIKE '%Đà Nẵng' OR dia_chi LIKE '%Quảng Trị'
-				AND (YEAR(CURDATE()) - YEAR(ngay_sinh) BETWEEN 18 AND 50 );
+WHERE dia_chi LIKE '%Đà Nẵng'
+   OR dia_chi LIKE '%Quảng Trị'
+    AND (YEAR(CURDATE()) - YEAR (ngay_sinh) BETWEEN 18 AND 50);
 
 -- 4.  Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được 
 -- sắp xếp tăng dần theo số lần đặt phòng của khách hàng. Chỉ đếm những khách hàng nào có Tên loại 
@@ -49,15 +49,14 @@ SELECT kh.ma_khach_hang,
        hd.ngay_lam_hop_dong,
        hd.ngay_ket_thuc,
        (
-               IFNULL(dv.chi_phi_thue, 0) + SUM(
-                   IFNULL(hdct.so_luong, 0) * IFNULL(dvdk.gia, 0)
-               )
+               IFNULL(dv.chi_phi_thue, 0) +
+               SUM(IFNULL(hdct.so_luong, 0) * IFNULL(dvdk.gia, 0))
            ) AS tong_tien
-FROM hop_dong hd
-         JOIN khach_hang kh ON hd.ma_khach_hang = kh.ma_khach_hang
-         JOIN loai_khach lk ON lk.ma_loai_khach = kh.ma_loai_khach
-         JOIN dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
-         JOIN hop_dong_chi_tiet hdct ON hdct.ma_hop_dong = hd.ma_hop_dong
-         JOIN dich_vu_di_kem dvdk ON dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+FROM loai_khach lk
+         JOIN khach_hang kh ON lk.ma_loai_khach = kh.ma_loai_khach
+         JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+         JOIN dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu
+         JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hd.ma_hop_dong
+         JOIN dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
 GROUP BY hd.ma_hop_dong
 ORDER BY kh.ma_khach_hang;
